7f936da52f38157df539beb3503169b2fc4b5de8
Team-Musafir <team.musafir50@gmail.com>
2025-05-18 14:54:06 +0530

    unprotected routes App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -32,9 +32,9 @@ function App() {
             <Route
               path="/dashboard"
               element={
-                <ProtectedRoute>
+               // <ProtectedRoute>
                   <Dashboard />
-                </ProtectedRoute>
+               // </ProtectedRoute>
               }
             />
           </Routes>
